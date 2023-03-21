1e240085812763097db81373ba1b2ae2a99031ca
John Victor <johnvcodes@proton.me>
2023-03-21 20:50:45 -0300

    Create authentication context.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { Outlet } from "react-router-dom";
 import Container from "./components/Container";
 import Header from "./components/Header";
+import AuthProvider from "./contexts/AuthContext";
 
 export default function App() {
   document.documentElement.classList.add("dark");
   return (
-    <Container>
-      <Header />
-      <Outlet />
-    </Container>
+    <AuthProvider>
+      <Container>
+        <Header />
+        <Outlet />
+      </Container>
+    </AuthProvider>
   );
 }
---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,42 @@
+import { onAuthStateChanged, User } from "firebase/auth";
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { auth } from "../firebase/config";
+
+type AuthContextType = {
+  user: User | null;
+};
+
+const AuthContext = createContext<AuthContextType | null>(null);
+
+export function useAuth() {
+  const currentAuthContext = useContext(AuthContext);
+  if (!currentAuthContext)
+    throw new Error("useAuth() must be used within <AuthProvider>!");
+  return currentAuthContext;
+}
+
+export default function AuthProvider({ children }: { children: ReactNode }) {
+  const [user, setUser] = useState<AuthContextType["user"]>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user }}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+}
