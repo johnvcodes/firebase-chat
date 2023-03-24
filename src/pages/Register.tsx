433c80bac971fa3466893939e428f48fa252f837
John Victor <johnvcodes@proton.me>
2023-03-24 18:39:49 -0300

    Add created user to database.

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,8 +1,14 @@
 import { ChangeEvent, FormEvent, useReducer } from "react";
-import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
-import { auth } from "../firebase/config";
-import { useNavigate } from "react-router-dom";
+import {
+  createUserWithEmailAndPassword,
+  updateProfile,
+  User,
+  UserCredential,
+} from "firebase/auth";
+import { auth, firestore } from "../firebase/config";
+import { Link, useNavigate } from "react-router-dom";
 import { AuthFormAction, AuthFormState } from "../types/auth-form";
+import { doc, serverTimestamp, setDoc } from "firebase/firestore";
 
 const initialState: AuthFormState = {
   username: "",
@@ -17,13 +23,17 @@ function registerReducer(
 ): AuthFormState {
   switch (action.type) {
     case "username":
-      return { ...state, username: action.payload };
+      return { ...state, username: action.payload as string };
     case "email":
-      return { ...state, email: action.payload };
+      return { ...state, email: action.payload as string };
     case "password":
-      return { ...state, password: action.payload };
+      return { ...state, password: action.payload as string };
     case "confirmPassword":
-      return { ...state, confirmPassword: action.payload };
+      return { ...state, confirmPassword: action.payload as string };
+    case "avatar":
+      return { ...state, avatar: action.payload as File };
+    default:
+      return state;
   }
 }
 
@@ -32,9 +42,12 @@ export default function Register() {
   const navigate = useNavigate();
 
   function handleChange(event: ChangeEvent<HTMLInputElement>) {
+    console.log(event.target.files);
+    let { name, files, value } = event.target;
+
     dispatch({
-      type: event.target.name as keyof AuthFormState,
-      payload: event.target.value,
+      type: name as keyof AuthFormState,
+      payload: (files && files[0]) || value,
     });
   }
 
@@ -43,22 +56,37 @@ export default function Register() {
     if (!state.email.includes("@")) return;
     if (state.password.length < 6) return;
     if (state.password !== state.confirmPassword) return;
+    let response: UserCredential;
+
     try {
-      const response = await createUserWithEmailAndPassword(
+      response = await createUserWithEmailAndPassword(
         auth,
         state.email,
         state.password
       );
+
       await updateProfile(response.user, { displayName: state.username });
     } catch (error) {
       return console.log(error);
     }
     navigate("/dashboard");
+
+    try {
+      await setDoc(doc(firestore, "users", response.user.uid), {
+        uid: response.user.uid,
+        displayName: state.username,
+        email: state.email,
+        createdAt: serverTimestamp(),
+      });
+    } catch (error) {
+      console.log(error);
+    }
   }
+
   return (
     <form
       onSubmit={handleSubmit}
-      className="m-auto grid w-80 gap-2 rounded border border-neutral-50 bg-neutral-50 p-2 shadow dark:border-neutral-700 dark:bg-neutral-800"
+      className="relative m-auto grid w-80 gap-2 rounded border border-neutral-50 bg-neutral-50 p-2 shadow dark:border-neutral-700 dark:bg-neutral-800"
     >
       <h2 className="w-fit justify-self-center rounded bg-neutral-900 p-1 font-bold uppercase tracking-widest">
         Crie sua conta
@@ -117,6 +145,24 @@ export default function Register() {
         placeholder="Mínimo de 6 dígitos"
         className="rounded bg-neutral-900 p-2"
       />
+      <label htmlFor="avatar" className="w-fit">
+        Foto de perfil
+      </label>
+      <input
+        onChange={handleChange}
+        accept=".jpg, .jpeg, .png"
+        type="file"
+        id="avatar"
+        name="avatar"
+        className="rounded bg-neutral-900 p-2"
+      />
+      <Link to="/login">Já possui uma conta? Entrar</Link>
+      <Link
+        to="/"
+        className="absolute -top-10 rounded border border-neutral-300 bg-neutral-50 p-1 dark:border-neutral-700 dark:bg-neutral-800"
+      >
+        Voltar
+      </Link>
       <button className="w-fit justify-self-center rounded bg-neutral-900 p-1">
         Confirmar
       </button>
