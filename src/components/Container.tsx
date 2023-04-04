cb5b2c201c1bba56e3830da2b7a7d31967b5e73d
John Victor <johnvcodes@proton.me>
2023-04-04 14:00:07 -0300

    Update container style with tailwind utility.

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -4,7 +4,7 @@ type Props = ComponentPropsWithoutRef<"div">;
 
 export default function Container({ children }: Props) {
   return (
-    <div className="m-auto flex h-4/5 w-4/5 flex-col overflow-hidden rounded border border-neutral-300 bg-neutral-50 dark:border-neutral-700 dark:bg-neutral-900 ">
+    <div className="container flex flex-col overflow-hidden border-x border-neutral-300 bg-neutral-50 dark:border-neutral-700 dark:bg-neutral-900 ">
       {children}
     </div>
   );
