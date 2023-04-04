506d141e2659f8d74974dd24adb3f0fce63fce34
John Victor <johnvcodes@proton.me>
2023-04-04 13:59:19 -0300

    Update contact sidebar and contact card.

--- src/components/ContactCard.tsx
+++ src/components/ContactCard.tsx
@@ -1,25 +1,36 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import { Contact } from "./Contacts";
 
 type Props = {
   id: string;
-  name: string;
-  online: boolean;
+  contact: Contact;
+  handleSelect(contact: Contact): void;
 };
 
-export default function ContactCard({ id, name, online }: Props) {
+export default function ContactCard({ id, contact, handleSelect }: Props) {
   return (
-    <div className="flex w-40 items-center gap-2 rounded border border-neutral-300 bg-neutral-800 p-2 dark:border-neutral-700">
+    <NavLink
+      onClick={() => handleSelect(contact)}
+      to={`/dashboard/${contact.data.displayName
+        .toLowerCase()
+        .split(" ")
+        .join("-")}`}
+      className={({ isActive }) =>
+        `
+        ${isActive && "bg-neutral-200 dark:bg-neutral-900"}
+          flex items-center gap-2 rounded border border-neutral-300 bg-neutral-50 p-2 hover:bg-neutral-100 dark:border-neutral-700 dark:bg-neutral-800 dark:hover:bg-neutral-700`
+      }
+    >
       <div className="flex h-8 w-8 items-center justify-center rounded-full bg-gradient-to-br from-amber-500 to-rose-500">
-        {name.slice(0, 1)}
+        {contact.data.displayName.slice(0, 1)}
       </div>
-      <div className="flex gap-2">
-        <h2 className="self-center">{name}</h2>
-        <span
-          className={`flex h-2 w-2 items-center justify-center rounded-full ${
-            online ? "bg-emerald-500" : "bg-rose-500"
-          }`}
-        ></span>
+      <div className="flex grow flex-col gap-2">
+        <h2 className="grow">{contact.data.displayName}</h2>
+        <span className="flex h-2 w-2 items-center text-sm">
+          {contact.lastMessage}
+        </span>
       </div>
-    </div>
+    </NavLink>
   );
 }
--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -1,21 +1,65 @@
+import { useEffect, useState } from "react";
+import { firestore } from "../firebase/config";
+import { doc, onSnapshot } from "firebase/firestore";
+import { useAuth } from "../contexts/AuthContext";
 import ContactCard from "./ContactCard";
 
-type Props = {};
+import { nanoid } from "nanoid";
+import { useChat } from "../contexts/ChatContext";
+import AddContact from "./AddContact";
+import SearchContact from "./SearchContact";
+import { Contact } from "../types/contact-type";
 
-export default function Contacts({}: Props) {
-  let contacts = [
-    { id: "1", name: "John", online: true },
-    { id: "2", name: "Yago", online: false },
-    { id: "3", name: "Andres", online: true },
-  ];
+export default function Contacts() {
+  const { user } = useAuth();
+  const { dispatch } = useChat();
+
+  const [contacts, setContacts] = useState<Contact[]>([]);
+  const [search, setSearch] = useState<string>("");
+
+  const filterContacts = Object.values(contacts).filter((item) => {
+    if (!search) return true;
+    return item.data.displayName.toLowerCase().includes(search.toLowerCase());
+  });
+
+  async function handleSelect(contact: Contact) {
+    if (!user) return;
+    dispatch({
+      type: "changedChat",
+      payload: { data: contact.data, lastMessage: "" },
+    });
+    setSearch("");
+  }
+
+  const getChats = () => {
+    if (!user) return;
+    let unsubscribe = onSnapshot(
+      doc(firestore, "chatRooms", user.uid),
+      (response) => {
+        if (!response.exists()) return;
+        setContacts(response.data() as Contact[]);
+      }
+    );
+
+    return () => {
+      unsubscribe();
+    };
+  };
+
+  useEffect(() => {
+    user && user.uid && getChats();
+  }, [user && user.uid]);
+
   return (
-    <div className="flex flex-col gap-2 p-2">
-      {contacts.map((contact) => (
+    <div className="flex w-fit flex-col gap-2 border-r border-neutral-300 p-2 dark:border-neutral-700">
+      <AddContact />
+      <SearchContact searchValue={search} setSearch={setSearch} />
+      {filterContacts.map((contact) => (
         <ContactCard
-          key={contact.id}
-          id={contact.id}
-          name={contact.name}
-          online={contact.online}
+          key={nanoid(16)}
+          id={nanoid(16)}
+          contact={contact}
+          handleSelect={handleSelect}
         />
       ))}
     </div>
