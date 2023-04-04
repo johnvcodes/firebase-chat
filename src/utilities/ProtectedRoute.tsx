29346fb56ee0684e9f97ecf39ae42c9b639478bf
John Victor <johnvcodes@proton.me>
2023-04-04 14:01:18 -0300

    Wrap protected route with chat context.

--- src/utilities/ProtectedRoute.tsx
+++ src/utilities/ProtectedRoute.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
+import { ChatProvider } from "../contexts/ChatContext";
 
 export default function ProtectedRoute() {
   const { user } = useAuth();
   let location = useLocation();
   if (!user) return <Navigate to="/" state={{ from: location }} />;
   return (
-    <>
+    <ChatProvider>
       <Outlet />
-    </>
+    </ChatProvider>
   );
 }
