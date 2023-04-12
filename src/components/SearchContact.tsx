0320c049c9927bea62425630733114213186ce16
John Victor <johnvcodes@proton.me>
2023-04-12 19:03:37 -0300

    Style change.

--- src/components/SearchContact.tsx
+++ src/components/SearchContact.tsx
@@ -1,3 +1,4 @@
+import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 import { Dispatch, SetStateAction } from "react";
 
 type Props = {
@@ -9,7 +10,7 @@ export default function SearchContact({ searchValue, setSearch }: Props) {
   return (
     <form
       onSubmit={(event) => event.preventDefault()}
-      className="flex items-center gap-2"
+      className="flex items-center rounded border border-neutral-400 bg-transparent text-neutral-900 transition-colors duration-300 placeholder:text-neutral-400 focus-within:border-neutral-600 focus-within:outline-none hover:border-neutral-600 dark:border-neutral-700 dark:text-neutral-50 dark:focus-within:border-neutral-500 dark:hover:border-neutral-500"
     >
       <label htmlFor="search-contact" className="sr-only">
         Buscar contato
@@ -21,8 +22,11 @@ export default function SearchContact({ searchValue, setSearch }: Props) {
         name="searchContact"
         id="search-contact"
         placeholder="Buscar contato"
-        className="rounded border border-neutral-400 bg-transparent p-2 text-neutral-900 transition-colors duration-300 placeholder:text-neutral-400 hover:border-neutral-600 focus:border-neutral-600 focus:outline-none dark:border-neutral-700 dark:text-neutral-50 dark:hover:border-neutral-500 dark:focus:border-neutral-500"
+        className="bg-transparent px-2 py-1 text-neutral-900 transition-colors duration-300 placeholder:text-neutral-400 focus:outline-none  dark:text-neutral-50"
       />
+      <span className="px-2 py-1">
+        <MagnifyingGlassIcon className="h-6 w-6" />
+      </span>
     </form>
   );
 }
