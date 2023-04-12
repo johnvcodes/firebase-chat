1a3032452001daa203081fd321e64d5c8739b738
John Victor <johnvcodes@proton.me>
2023-04-12 16:37:56 -0300

    Style update.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,12 @@
 import { signOut } from "firebase/auth";
-import { useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 import { auth } from "../firebase/config";
 import ThemeButton from "./ThemeButton";
 
 export default function Header() {
   const { user } = useAuth();
-  const navigate = useNavigate();
-  async function logout() {
+
+  async function handleLogout() {
     try {
       await signOut(auth);
     } catch (error) {
@@ -15,19 +14,17 @@ export default function Header() {
     }
   }
   return (
-    <header className="flex items-center justify-between border-b border-neutral-300 bg-neutral-50 p-2 dark:border-neutral-700 dark:bg-neutral-900">
-      <div className="flex items-center gap-2">
-        <h2 className="bg-gradient-to-r from-amber-500 to-rose-500 bg-clip-text font-bold uppercase tracking-widest text-transparent">
-          Firebase Chat
-        </h2>
-      </div>
+    <header className="flex items-center justify-between border-b border-neutral-400 p-2 dark:border-neutral-700">
+      <h2 className="uppercase tracking-widest">Talq</h2>
       <div className="flex items-center gap-2">
         <ThemeButton />
         {user && (
-          <>
-            {user.displayName && <p>{user.displayName}</p>}
-            <button onClick={logout}>Sair</button>
-          </>
+          <button
+            onClick={handleLogout}
+            className="rounded px-2 py-1 transition-colors duration-300 hover:bg-neutral-300 dark:hover:bg-neutral-700"
+          >
+            Sair
+          </button>
         )}
       </div>
     </header>
