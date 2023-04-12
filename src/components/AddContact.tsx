b4dd125b378c8a374d1ffc147929e90084aaafd6
John Victor <johnvcodes@proton.me>
2023-04-12 19:03:24 -0300

    Add notification to add contact.

--- src/components/AddContact.tsx
+++ src/components/AddContact.tsx
@@ -11,12 +11,14 @@ import {
 } from "firebase/firestore";
 import { firestore } from "../firebase/config";
 import { useAuth } from "../contexts/AuthContext";
+import { UserPlusIcon } from "@heroicons/react/24/solid";
 
 type Props = {};
 
 export default function AddContact({}: Props) {
   const { user } = useAuth();
   const [input, setInput] = useState<string>("");
+  const [status, setStatus] = useState("");
 
   async function handleAddContact(event: FormEvent) {
     event.preventDefault();
@@ -28,11 +30,12 @@ export default function AddContact({}: Props) {
     );
 
     setInput("");
-
+    setStatus("Pedido enviado");
     let response = await getDocs(usersQuery);
 
     response.forEach(async (item) => {
       if (!item.exists()) return;
+
       try {
         let combinedId =
           user.uid > item.data().uid
@@ -61,27 +64,31 @@ export default function AddContact({}: Props) {
       } catch (error) {
         console.log(error);
       }
+      setStatus("");
     });
   }
 
   return (
-    <form
-      onSubmit={handleAddContact}
-      className="flex items-center gap-2 rounded"
-    >
-      <input
-        onChange={(event) => setInput(event.target.value)}
-        value={input}
-        type="email"
-        name="addContact"
-        id="add-contact"
-        placeholder="Adicionar contato"
-        required
-        className="rounded border border-neutral-400 bg-transparent p-2 text-neutral-900 transition-colors duration-300 placeholder:text-neutral-400 hover:border-neutral-600 focus:border-neutral-600 focus:outline-none dark:border-neutral-700 dark:text-neutral-50 dark:hover:border-neutral-500 dark:focus:border-neutral-500"
-      />
-      <label htmlFor="add-contact" className="sr-only">
-        Adicionar contato
-      </label>
+    <form onSubmit={handleAddContact} className="flex flex-col gap-2">
+      <div className="flex items-center rounded border border-neutral-400 bg-transparent text-neutral-900 transition-colors duration-300 placeholder:text-neutral-400 focus-within:border-neutral-600 focus-within:outline-none hover:border-neutral-600 dark:border-neutral-700 dark:text-neutral-50 dark:focus-within:border-neutral-500 dark:hover:border-neutral-500">
+        <input
+          onChange={(event) => setInput(event.target.value)}
+          value={input}
+          type="email"
+          name="addContact"
+          id="add-contact"
+          placeholder="E-mail do contato"
+          required
+          className="bg-transparent px-2 py-1 text-neutral-900 transition-colors duration-300 placeholder:text-neutral-400 focus:outline-none  dark:text-neutral-50"
+        />
+        <label htmlFor="add-contact" className="sr-only">
+          E-mail do contato
+        </label>
+        <button className="px-2 py-1 transition-colors duration-300 hover:bg-neutral-500">
+          <UserPlusIcon className="h-6 w-6" />
+        </button>
+      </div>
+      {status && <span className="w-1/2 text-xs text-blue-500">{status}</span>}
     </form>
   );
 }
