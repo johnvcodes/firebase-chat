bf9e9e5d41c8ec8182d89c46f5c5e1128fd9394d
John Victor <johnvcodes@proton.me>
2023-04-12 16:37:38 -0300

    Create theme handling.

--- src/components/ThemeButton.tsx
+++ src/components/ThemeButton.tsx
@@ -1,17 +1,42 @@
-import { SunIcon } from "@heroicons/react/24/outline";
+import { useEffect, useState } from "react";
+import { MoonIcon, SunIcon } from "@heroicons/react/24/solid";
 
 export default function ThemeButton() {
+  const [darkMode, setDarkMode] = useState<boolean>(false);
   function handleTheme() {
-    document.documentElement.classList.toggle("dark");
+    setDarkMode(!darkMode);
   }
+
+  useEffect(() => {
+    if (
+      localStorage.theme === "dark" ||
+      (!localStorage.theme &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      return setDarkMode(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (darkMode === true) {
+      localStorage.setItem("theme", "dark");
+      return document.documentElement.classList.add("dark");
+    }
+    localStorage.setItem("theme", "light");
+    return document.documentElement.classList.remove("dark");
+  }, [darkMode]);
 
   return (
     <button
       onClick={handleTheme}
       type="button"
-      className="w-fit rounded border border-neutral-300 p-2 dark:border-neutral-700"
+      className="rounded p-1 transition-colors duration-300 hover:bg-neutral-300 dark:hover:bg-neutral-700"
     >
-      <SunIcon className="h-6 w-6" />
+      {darkMode ? (
+        <SunIcon className="h-6 w-6 " />
+      ) : (
+        <MoonIcon className="h-6 w-6" />
+      )}
     </button>
   );
 }
